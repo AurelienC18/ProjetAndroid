9e12df890a6a9adb7011a9b15b95e308a7abe4df
AlexisLambois <lambois16@gmail.com>
2019-06-07 11:25:19 +0200

    add light func

---
+++ app/src/main/java/com/example/myapp/LampeTorche.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lampetorche : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lampetorche)
+    }
+}
