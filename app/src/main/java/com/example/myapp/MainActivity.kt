82965cba0ae1b867fa78b6ec0fa989ae474ac284
Aurélien <aurelienc18@gmail.com>
2019-06-07 13:47:52 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Math.random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,15 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        button.setOnClickListener {
-            cpt++;
-            textView.text = "${cpt}"
-        }
-
-        buttonReset.setOnClickListener {
-            cpt = 0
-            textView.text = "${cpt}"
-        }
     }
 }
