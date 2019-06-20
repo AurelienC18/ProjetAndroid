d37d271a98cfe76bf589f5bad51f3c297ea21f62
AlexisLambois <lambois16@gmail.com>
2019-06-20 11:11:57 +0200

    conflit 1

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.myapp
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var lightState = true
+    var cameraManager: CameraManager? = null
+    var camera: Camera? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +29,63 @@ class MainActivity : AppCompatActivity() {
             val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
             ActivityCompat.requestPermissions(this, permissions,0)
         }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            val cameraList = cameraManager!!.cameraIdList
+            for (str in cameraList)
+                println(str)
+        } catch (e: Exception) {
+            Log.e("error", e.message)
+        }
     }
 
-    fun toLamp(view: View) {
-        val intent = Intent(this, LampeTorche::class.java)
-        // start your next activity
-        startActivity(intent)
+    fun switchLight(view: View) {
+        lightState = !lightState
+        goSwitch()
+    }
+
+    private fun goSwitch() {
+        if (lightState) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                try {
+                    cameraManager!!.setTorchMode("0", false)
+                } catch (e: Exception) {
+                    Log.e("error", e.message)
+                }
+            } else {
+                if (camera != null) {
+                    camera!!.stopPreview()
+                    camera!!.release()
+                    camera = null
+                }
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                try {
+                    cameraManager!!.setTorchMode("0", true)
+                } catch (e: Exception) {
+                    Log.e("error", e.message)
+                }
+            } else {
+                val pm = packageManager
+                val features = pm.systemAvailableFeatures
+
+                for(f in features) {
+                    if(PackageManager.FEATURE_CAMERA_FLASH==f.name) {
+                        if (null == camera){
+                            camera = Camera.open()
+                        }
+
+                        val parameters = camera!!.parameters
+                        parameters.flashMode = Camera.Parameters.FLASH_MODE_TORCH
+                        camera!!.parameters = parameters
+                        camera!!.startPreview()
+
+                    }
+                }
+            }
+        }
     }
 
     fun toRecorder(view: View) {
--- app/src/main/java/com/example/myapp/VoiceRecorder.kt
+++ app/src/main/java/com/example/myapp/VoiceRecorder.kt
@@ -4,16 +4,27 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_voice_recorder.*
+import java.io.File
 import java.io.IOException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import android.widget.AdapterView
+import java.nio.file.Files
+
 
 class VoiceRecorder : AppCompatActivity() {
 
@@ -26,13 +37,22 @@ class VoiceRecorder : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_voice_recorder)
 
-        output = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
-        mediaRecorder = MediaRecorder()
+        val wallpaperDirectory = File(Environment.getExternalStorageDirectory().absolutePath + "/Voice")
+        // have the object build the directory structure, if needed.
+        wallpaperDirectory.mkdirs()
 
+        this.refreshFilesDirectory()
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+        val formatted = current.format(formatter)
+        output = Environment.getExternalStorageDirectory().absolutePath + "/Voice/" + formatted + "_recording.mp3"
+        mediaRecorder = MediaRecorder()
         mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
         mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
         mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
         mediaRecorder?.setOutputFile(output)
+
 
         if (ContextCompat.checkSelfPermission(
                 this,
@@ -79,8 +99,50 @@ class VoiceRecorder : AppCompatActivity() {
         }
     }
 
+    private fun refreshFilesDirectory() {
+        val filesList = File(Environment.getExternalStorageDirectory().absolutePath + "/Voice").listFiles()
+
+        val lv = findViewById<ListView>(R.id.products)
+        val prodAdapter = ArrayAdapter<File>(
+            this,
+            android.R.layout.simple_list_item_1, filesList
+        )
+        lv.adapter = prodAdapter
+
+        lv.setOnItemClickListener { parent, view, position, id ->
+            val element = prodAdapter.getItem(position)
+            val data = Uri.parse (element.absolutePath)
+            val mp = MediaPlayer.create (this, data)
+            mp.start ()
+        }
+
+        lv.onItemLongClickListener = object : AdapterView.OnItemLongClickListener {
+            override fun onItemLongClick(
+                arg0: AdapterView<*>, arg1: View,
+                pos: Int, id: Long
+            ): Boolean {
+                val element = prodAdapter.getItem(pos)
+                element.delete()
+                refreshFilesDirectory()
+                return true
+            }
+        };
+
+    }
+
     private fun startRecording() {
         try {
+            val current = LocalDateTime.now()
+
+            val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+            val formatted = current.format(formatter)
+            output = Environment.getExternalStorageDirectory().absolutePath + "/Voice/" + formatted + "_recording.mp3"
+            mediaRecorder = MediaRecorder()
+            mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+
+            mediaRecorder?.setOutputFile(output)
             mediaRecorder?.prepare()
             mediaRecorder?.start()
             state = true
@@ -97,6 +159,7 @@ class VoiceRecorder : AppCompatActivity() {
             mediaRecorder?.stop()
             mediaRecorder?.release()
             state = false
+            this.refreshFilesDirectory()
         } else {
             Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
@@ -105,13 +168,13 @@ class VoiceRecorder : AppCompatActivity() {
     @SuppressLint("RestrictedApi", "SetTextI18n")
     @TargetApi(Build.VERSION_CODES.N)
     private fun pauseRecording() {
-        if(state) {
-            if(!recordingStopped){
-                Toast.makeText(this,"Stopped!", Toast.LENGTH_SHORT).show()
+        if (state) {
+            if (!recordingStopped) {
+                Toast.makeText(this, "Stopped!", Toast.LENGTH_SHORT).show()
                 mediaRecorder?.pause()
                 recordingStopped = true
                 button_pause_recording.text = "Resume"
-            }else{
+            } else {
                 resumeRecording()
             }
         }
@@ -120,7 +183,7 @@ class VoiceRecorder : AppCompatActivity() {
     @SuppressLint("RestrictedApi", "SetTextI18n")
     @TargetApi(Build.VERSION_CODES.N)
     private fun resumeRecording() {
-        Toast.makeText(this,"Resume!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Resume!", Toast.LENGTH_SHORT).show()
         mediaRecorder?.resume()
         button_pause_recording.text = "Pause"
         recordingStopped = false
